57f6e3a5abf6ea170f5c9832e41708403a0316e5
Qian Qiao <qian.qiao@gmail.com>
2021-06-02 07:49:11 +0000

    Skipping the seperate typescript compilation step instead to have rollup watch all files.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { createSpaConfig } from '@open-wc/building-rollup';
 import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
 import merge from 'deepmerge';
 
 const baseConfig = createSpaConfig({
@@ -15,8 +16,9 @@ export default merge(baseConfig, {
     input: './index.html',
     plugins: [
         replace({
-            'process.env.NODE_ENV': JSON.stringify(!!production ? 'production' : 'development'),
+            'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
             'process.env.BUILD_TYPE': JSON.stringify(buildType)
-        })
+        }),
+        typescript()
     ]
 });
