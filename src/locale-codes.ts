b992fb9de8792296935bbaa6101f5a454e41c0a4
Qian Qiao <qqiao@users.noreply.github.com>
2021-10-06 04:13:40 +0800

    Clipboard API. (#78)
    We are now using the clipboard API whenever possible

--- src/locale-codes.ts
+++ src/locale-codes.ts
@@ -4,20 +4,20 @@
 /**
  * The locale code that templates in this source code are written in.
  */
-export const sourceLocale = 'en';
+export const sourceLocale = `en`;
 
 /**
  * The other locale codes that this application is localized into. Sorted
  * lexicographically.
  */
 export const targetLocales = [
-  'zh',
+  `zh`,
 ] as const;
 
 /**
  * All valid project locale codes. Sorted lexicographically.
  */
 export const allLocales = [
-  'en',
-  'zh',
+  `en`,
+  `zh`,
 ] as const;
